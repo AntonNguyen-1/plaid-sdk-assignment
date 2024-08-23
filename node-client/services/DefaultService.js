f1240a035d4065a1881f0e146ab739d66112335f
AntonNguyen-1 <anton.zxwxc@gmail.com>
2024-08-23 19:51:11 +0300

    add plaid config and client; add institution search endpoint;

--- node-client/services/DefaultService.js
+++ node-client/services/DefaultService.js
@@ -5,13 +5,17 @@ const Service = require('./Service');
 * list institutions
 *
 * institutionsSearchPostRequest InstitutionsSearchPostRequest 
+* pLAIDCLIENTID String  (optional)
+* pLAIDSECRET String  (optional)
 * returns _institutions_search_post_200_response
 * */
-const institutionsSearchPOST = ({ institutionsSearchPostRequest }) => new Promise(
+const institutionsSearchPOST = ({ institutionsSearchPostRequest, pLAIDCLIENTID, pLAIDSECRET }) => new Promise(
   async (resolve, reject) => {
     try {
       resolve(Service.successResponse({
         institutionsSearchPostRequest,
+        pLAIDCLIENTID,
+        pLAIDSECRET,
       }));
     } catch (e) {
       reject(Service.rejectResponse(
